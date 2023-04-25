52dd83eb6f3fa04eae3166dc0c48b8d543609696
Rutujaj1911 <116167421+Rutujaj1911@users.noreply.github.com>
2023-04-26 00:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,9 +13,37 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
 
-  return ;//return decodedArr
+    // Check if the character is an uppercase letter
+    if (charCode >= 65 && charCode <= 90) {
+      // Shift the character code by 13 positions
+      var newCharCode = ((charCode - 65 + 13) % 26) + 65;
+
+      // Add the decoded character to the array
+      decoded.push(String.fromCharCode(newCharCode));
+    } else if (charCode >= 97 && charCode <= 122) {
+      // Check if the character is a lowercase letter
+      // Shift the character code by 13 positions
+      var newCharCode = ((charCode - 97 + 13) % 26) + 97;
+
+      // Add the decoded character to the array
+      decoded.push(String.fromCharCode(newCharCode));
+    } else {
+      // If the character is not a letter, add it to the array as-is
+      decoded.push(str.charAt(i));
+    }
+  }
+
+  // Join the array of decoded characters into a string and return it
+  return decoded.join("");
 }
+
+
+
+
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
